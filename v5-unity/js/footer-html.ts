40ba77a424959af93bb4e17c279b558eb64a4f78
Philip Guo <philip@pgbovine.net>
2019-10-24 22:01:40 -0700

    minor wording fix

--- v5-unity/js/footer-html.ts
+++ v5-unity/js/footer-html.ts
@@ -69,8 +69,8 @@ JavaScript ES6 and TypeScript 1.4.1 (Node.js v6.0.0), Ruby 2 (MRI 2.2.2), C and 
 
 
 export var nullTraceErrorLst = [
-  "Unknown error: The server may be OVERLOADED now; try again later.",
-  'Or your code contains <a target="_blank" href="https://github.com/pgbovine/OnlinePythonTutor/blob/master/unsupported-features.md">UNSUPPORTED FEATURES</a>.',
-  "This site is free with NO available technical support. [#NullTrace]"];
+  "Unknown error: The server is OVERLOADED or your code has UNSUPPORTED FEATURES.",
+  "Try again later. This site is free with NO available technical support. [#NullTrace]"
+];
 
 export var unsupportedFeaturesStr = 'see <a target="_blank" href="https://github.com/pgbovine/OnlinePythonTutor/blob/master/unsupported-features.md">UNSUPPORTED FEATURES</a>';
